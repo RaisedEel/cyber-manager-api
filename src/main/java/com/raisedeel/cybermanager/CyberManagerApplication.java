76a5e43ba4d1351fe8f16701114a8c663ccaf012
Antonio Aguilera <antonhy.romero98@gmail.com>
2023-03-17 14:22:06 -0600

    Inicio del Proyecto. Creado usando Spring Initializer.

---
+++ src/main/java/com/raisedeel/cybermanager/CyberManagerApplication.java
@@ -0,0 +1,13 @@
+package com.raisedeel.cybermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisedeel/cybermanager/CyberManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisedeel.cybermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
