fcf33211955ad0656ff83559468ec9c19e7cdbd4
Antonio Aguilera <antonhy.romero98@gmail.com>
2023-03-17 15:25:31 -0600

    Computer creado para representar la entidad.

---
+++ src/main/java/com/raisedeel/cybermanager/model/Computer.java
@@ -0,0 +1,48 @@
+package com.raisedeel.cybermanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "computer")
+public class Computer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private long id;
+
+  @NonNull
+  @Column(name = "name")
+  private String name;
+
+  @NonNull
+  @Column(name = "price")
+  private int price;
+
+  @NonNull
+  @Column(name = "brand")
+  private String brand;
+
+  @NonNull
+  @Column(name = "antiquity")
+  private int antiquity;
+
+  @NonNull
+  @Column(name = "revision")
+  private Date revision;
+
+  @NonNull
+  @Column(name = "serial")
+  private String serial;
+
+  @NonNull
+  @Column(name = "description")
+  private String description;
+}
